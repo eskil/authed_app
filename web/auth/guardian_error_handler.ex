914c44a2190cfa9ccd1df5ea202f707fa34c5eff
Eskil Heyn Olsen <eskil@eskil.org>
2017-03-11 17:33:05 -0800

    make guardian auth handler a controller and handle unauthenticated api

--- web/auth/guardian_error_handler.ex
+++ web/auth/guardian_error_handler.ex
@@ -1,10 +1,21 @@
 defmodule AuthedApp.GuardianErrorHandler do
+  use AuthedApp.Web, :controller
   import AuthedApp.Router.Helpers
-  import Phoenix.Controller
+  #import Phoenix.Controller
 
-  def unauthenticated(conn, _params) do
+  def handle_unauthenticated(conn, "json") do
+    conn
+    |> put_status(:forbidden)
+    |> json(%{})
+  end
+
+  def handle_unauthenticated(conn, "html") do
     conn
     |> put_flash(:error, "You must be signed in to access this page.")
     |> redirect(to: session_path(conn, :new))
+  end
+
+  def unauthenticated(conn, _params) do
+    handle_unauthenticated(conn, get_format(conn))
   end
 end
