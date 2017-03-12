b72369f59f295cf3d28db265569168e1e904759a
Eskil Heyn Olsen <eskil@eskil.org>
2017-03-11 17:40:55 -0800

    Make user controller use the new login

--- web/auth/auth.ex
+++ web/auth/auth.ex
@@ -4,23 +4,27 @@ defmodule AuthedApp.Auth do
   alias AuthedApp.Repo
   alias AuthedApp.User
 
-  def login(conn, user) do
+  def login(conn, user, "html") do
     conn
     |> Guardian.Plug.sign_in(user)
+  end
+
+  def login(conn, user, "json") do
+    conn
+    |> Guardian.Plug.api_sign_in(user)
   end
 
   def logout(conn) do
     Guardian.Plug.sign_out(conn)
   end
 
-  def login_by_email_and_password(conn, email, password) do
+  def login_by_email_and_password(conn, email, password, opts \\ [format: "html"]) do
     # Get user by email
     user = Repo.get_by(User, email: email)
-
     cond do
       # We have a user and the hashed password matches the db one.
       user && checkpw(password, user.password_hash) ->
-        {:ok, login(conn, user)}
+        {:ok, login(conn, user, opts[:format])}
       # We have a user but the password check failed.
       user ->
         {:error, :unauthorized, conn}
